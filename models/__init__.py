ed82f1987a252b439b2d5e278da885b00739eea5
Ayrton Denner <ayrtondenner_2013@hotmail.com>
2026-01-08 16:22:45 -0300

    Added Jupyter notebook analysis and more properties to review entity

--- models/__init__.py
+++ models/__init__.py
@@ -38,9 +38,29 @@ class TripAdvisorReview():
     @property
     def review_len(self) -> int:
         return len(self.review)
+    
+    @property
+    def day_of_month(self) -> int:
+        return self.data_postagem.day
+    
+    @property
+    def day_of_week(self) -> str:
+        return self.data_postagem.strftime("%A").lower()
+
+    @property
+    def month(self) -> str:
+        return self.data_postagem.strftime("%B").lower()
+    
+    @property
+    def year(self) -> int:
+        return self.data_postagem.year
 
     def to_dict(self) -> dict:
         d = dict(self.__dict__)
         d["title_len"] = self.title_len
         d["review_len"] = self.review_len
+        d["day_of_month"] = self.day_of_month
+        d["day_of_week"] = self.day_of_week
+        d["month"] = self.month
+        d["year"] = self.year
         return d
